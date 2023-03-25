2e185452cdd422fd78d222d96788f4c1ac86ee84
Liang Wang <lw6@stellar-intel.princeton.edu>
2023-03-25 15:37:54 -0400

    add casted callback cb

--- main.lua
+++ main.lua
@@ -12,30 +12,33 @@ ffi.cdef[[
 
 local lib = ffi.load("./libmylib.so")
 
-local my_type = ffi.typeof("struct MyStruct")
-
-local my_mt = {
-   __new = function(self, lua_func)
-      local obj = ffi.new(MyStruct)
-      obj.callback = lua_func
-      return obj
-   end,
-}
-
-MyStruct = ffi.metatype(my_type, my_mt)
-
 local my_lua_func = function(inp)
    print("calling my_lua_func on", inp)
 end
 
+-- lua wrapper of c struct, which implicitly converts my_lua_func
+local my_type = ffi.typeof("struct MyStruct")
+local my_mt = {
+   __new = function(self, lua_func)
+      local obj = ffi.new(MyStruct)
+      obj.callback = lua_func -- implicit conversion
+      return obj
+   end,
+}
+MyStruct = ffi.metatype(my_type, my_mt)
 local obj = MyStruct(my_lua_func)
-
 print(obj, obj.callback)
 
-for c=1,2 do
-   print("")
-   lib.call_func_ptr(obj, c)
-   print("")
+-- isolated callback from cast
+local cb = ffi.cast("FuncPtr", my_lua_func)
+
+for c=1,3 do
+   print("\n>>>>>>>>>>")
+   -- following fails if only obj or cb is created
+   lib.call_func_ptr(obj, c) -- OK if obj is created after cb
+   -- obj.callback(c) -- OK if obj is created after cb
+   -- cb(c) -- OK if cb is created after obj
+   print("<<<<<<<<<<\n")
 end
 
 print("done")
